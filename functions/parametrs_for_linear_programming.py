84c7a4a2e9ee3aa5aa6fda03784987fbc404be48
aliyakalykulova <110055731+aliyakalykulova@users.noreply.github.com>
2026-07-27 19:19:40 +0500

    Update parametrs_for_linear_programming.py

--- functions/parametrs_for_linear_programming.py
+++ functions/parametrs_for_linear_programming.py
@@ -16,41 +16,58 @@ def maximize_function(food_df, nutrient_preds):
 # ---- Ограничения зависят от их роли как преимущественного источника нутриента
 # ---- Отображение ограничений в виде ползунков (пользователь может корректировать вручную)
 def ingredients_limits(ingredirents_df, ingredient_names):
-
-   # --- Формирование списков ингредиентов по источникам основных нутриентов 	
-   proteins=ingredirents_df[ingredirents_df["category_ru"].isin(["Яйца и молочные продукты", "Мясо"])]["ingredient_format_cat"].tolist()
-   oils=ingredirents_df[ingredirents_df["category_ru"].isin([ "Масло и жир"])]["ingredient_format_cat"].tolist()
-   carbonates_cer=ingredirents_df[ingredirents_df["category_ru"].isin(["Крупы"])]["ingredient_format_cat"].tolist()
-   carbonates_veg=ingredirents_df[ingredirents_df["category_ru"].isin(["Зелень и специи","Овощи и фрукты"])]["ingredient_format_cat"].tolist()
-   other=ingredirents_df[ingredirents_df["category_ru"].isin(["Вода, соль и сахар","Дополнительные пищевые компоненты"])]["ingredient_format_cat"].tolist()
-
+   st.subheader("🌿 Рекомендуемые ингредиенты")
+   for ing in ingredient_names:
+      st.write("• " + ing.replace("— Обыкновенный",""))
+	   
+   proteins=ingredirents_df[ingredirents_df["category_ru"].isin(["Яйца и молочные продукты", "Мясо"])]["full_name_ingredient"].tolist()
+   oils=ingredirents_df[ingredirents_df["category_ru"].isin([ "Масло и жир"])]["full_name_ingredient"].tolist()
+   carbonates_cer=ingredirents_df[ingredirents_df["category_ru"].isin(["Крупы"])]["full_name_ingredient"].tolist()
+   carbonates_veg=ingredirents_df[ingredirents_df["category_ru"].isin(["Овощи и фрукты"])]["full_name_ingredient"].tolist()
+   carbonates_grace=ingredirents_df[ingredirents_df["category_ru"].isin(["Зелень и специи"])]["full_name_ingredient"].tolist()
+   other=ingredirents_df[ingredirents_df["category_ru"].isin(["Вода, соль и сахар"])]["full_name_ingredient"].tolist()
+	
    st.subheader("Ограничения по количеству ингредиентов (в % от 100 г):")
-   ingr_ranges = []	
+   ingr_ranges = []
    for ingr in ingredient_names:
       if ingr in proteins:
-         ingr_ranges.append(st.slider(f"{ingr.replace(" — Обыкновенный", "")}", 0, 100, (40,60)))
+         ingr_ranges.append(st.slider(f"{ingr.replace(" — Обыкновенный", "")}", 0, 100, (50,90)))
       elif ingr in oils:
          ingr_ranges.append(st.slider(f"{ingr.replace(" — Обыкновенный", "")}", 0, 100, (1,10)))
       elif ingr in carbonates_cer:
          ingr_ranges.append(st.slider(f"{ingr.replace(" — Обыкновенный", "")}", 0, 100, (5,35)))
       elif ingr in carbonates_veg:
          ingr_ranges.append(st.slider(f"{ingr.replace(" — Обыкновенный", "")}", 0, 100, (5,25)))
-      elif "Вода" in ingr:
+      elif "WATER" in ingr:
          ingr_ranges.append(st.slider(f"{ingr.replace(" — Обыкновенный", "")}", 0, 100, (0,30)))
       elif ingr in other:
          ingr_ranges.append(st.slider(f"{ingr.replace(" — Обыкновенный", "")}", 0, 100, (1,3)))
    return ingr_ranges
 
+
 # ---- Установка ограничений (min, max) для основных нутриентов
 # ---- Отображение ограничений в виде ползунков (ручная корректировка пользователем)
-def nutrients_limits(nutrient_preds):
+def nutrients_limits(food_df):
    st.subheader("Ограничения по нутриентам:")
    nutr_ranges = {}
-   nutr_ranges['moisture_per'] = st.slider(f"{'Влага'}", 0, 100, (int(nutrient_preds["moisture"]-5), int(nutrient_preds["moisture"]+5)))
-   nutr_ranges['protein_per'] = st.slider(f"{'Белки'}", 0, 100, (int(nutrient_preds["protein"]-3), int(nutrient_preds["protein"]+6)))
-   nutr_ranges['carbohydrate_per'] = st.slider(f"{'Углеводы'}", 0, 100, (int(nutrient_preds["carbohydrate"]-4), int(nutrient_preds["carbohydrate"]+4)))
-   nutr_ranges['fats_per'] = st.slider(f"{'Жиры'}", 0, 100, (int(nutrient_preds["fats"]-1), int(nutrient_preds["fats"]+1)) )
-   return nutr_ranges				               
+   nutr_ranges['moisture_per'] = st.slider(f"{'Влага'}", 0, 100, (65, 95))
+	
+   s = food_df[(food_df["food_form"] == "wet food") &(food_df["moisture_per"] > 0.5) ]["protein_per"]
+   protein_min=(100-nutr_ranges['moisture_per'][0])*0.25
+   protein_min=protein_min if protein_min > s.mean()-s.std() else s.mean()-s.std()
+   nutr_ranges['protein_per'] = st.slider(f"{'Белки'}", 0, 100, (int(protein_min), 30))
+	
+   s = food_df[(food_df["food_form"] == "wet food") &(food_df["moisture_per"] > 0.5) ]["fats_per"]
+   fats_min= (100-nutr_ranges['moisture_per'][0])*0.085
+   fats_min=fats_min if fats_min > s.mean()-s.std() else s.mean()-s.std()
+   nutr_ranges['fats_per'] = st.slider(f"{'Жиры'}", 0, 100, (int(fats_min),15) )
+   
+   carb_max=100-nutr_ranges['protein_per'][0]-nutr_ranges['fats_per'][0]-nutr_ranges['moisture_per'][0]
+   nutr_ranges['carbohydrate_per'] = st.slider(f"{'Углеводы'}", 0, 100, (5, int(carb_max)))
+	
+   return nutr_ranges
+
+
 
 # ---- Подготовка параметров задачи линейного программирования
 def lin_prog_parametrs(food,ingredient_names,nutr_ranges,ingr_ranges,maximaze_nutrs,nutrients_transl):
